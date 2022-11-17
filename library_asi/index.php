0075f03c6b46a37eafba2108e8d09b7bc942b427
MarkCayapas <cayapas.mark@gmail.com>
2022-11-17 10:17:00 +0800

    color change

--- library_asi/index.php
+++ library_asi/index.php
@@ -9,7 +9,79 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
       <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
       <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="style.css">
+      <style>
+                  .container,
+            .row,
+            .h2 {
+                text-align: center;
+            }
+
+            .login-container {
+                margin-top: 5%;
+                margin-bottom: 5%;
+            }
+
+            .login-form-1 {
+                padding: 5%;
+                background: #41A83F;
+                box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
+            }
+
+            .login-form-1 h3 {
+                text-align: center;
+                color: #333;
+            }
+
+            .login-form-2 {
+                padding: 5%;
+                background: #41A83F;
+                box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
+            }
+
+            .login-form-2 h3 {
+                text-align: center;
+                color: #333;
+            }
+
+            .login-container form {
+                padding: 10%;
+            }
+
+            .btnSubmit {
+                width: 50%;
+                border-radius: 1rem;
+                padding: 1.5%;
+                border: none;
+                cursor: pointer;
+            }
+
+            .login-form-1 .btnSubmit {
+                font-weight: 600;
+                color: #333
+                    ;
+                background-color: #009D12;
+            }
+
+            .login-form-2 .btnSubmit {
+                font-weight: 600;
+                color: #333;
+                background-color: #009D12;
+            }
+
+            .login-form-2 .ForgetPwd {
+                color: #fff;
+                font-weight: 600;
+                text-decoration: none;
+            }
+
+            .login-form-1 .ForgetPwd {
+                color: #0062cc;
+                font-weight: 600;
+                text-decoration: none;
+            }
+  
+      </style>
+        
     </head>
     
     <body>
